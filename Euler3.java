928712ecaaf42e1859e88be386e932f0073a67fa
graeme <graemenathan@gmail.com>
2014-01-21 13:29:21 -0800

    formatting

--- Euler3.java
+++ Euler3.java
@@ -2,7 +2,10 @@ import java.util.*;
 import java.lang.Math.*;
 public class Euler3{
     public static long Sieve(long max){
-/*Start at 2, iterate up to sqrt(max) checking if each number divides max. If it divides max then redefine max as the reslt of that division. Repeat until you can no longer divide max. Max will not be the largest prime factor*/
+/*Start at 2, iterate up to sqrt(max) checking if each 
+number divides max. If it divides max then redefine max 
+as the result of that division. Repeat until you can no 
+longer divide max. Max will not be the largest prime factor*/
         for(int i = 2; i*i <= max;i++){
             while(max%i == 0){
                 max = max/i;
