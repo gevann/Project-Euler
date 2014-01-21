7d7ac5a00ded31883cbb657c3e913ed22659b815
graeme <graemenathan@gmail.com>
2014-01-21 13:27:12 -0800

    added comments, removed unnecessary lines of code

--- Euler3.java
+++ Euler3.java
@@ -1,18 +1,14 @@
 import java.util.*;
 import java.lang.Math.*;
 public class Euler3{
-    //600 851 475 143
     public static long Sieve(long max){
-        long lrgst=1;
+/*Start at 2, iterate up to sqrt(max) checking if each number divides max. If it divides max then redefine max as the reslt of that division. Repeat until you can no longer divide max. Max will not be the largest prime factor*/
         for(int i = 2; i*i <= max;i++){
             while(max%i == 0){
                 max = max/i;
-                lrgst = i;
             }
         }
-        if(max > lrgst)
-            return max;
-        return lrgst;
+        return max;
     }
 
     public static void main(String [] args){
